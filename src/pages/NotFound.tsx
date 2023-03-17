f3ffb36a7ba27c699b392f5be713743d071c7385
Artem Mokhov <teumik@gmail.com>
2023-03-17 20:42:24 +0400

    feat: implement NotFound component

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <>
+        <h1>Page not found</h1>
+        <p>Footer</p>
+      </>
+    );
+  }
+}
+
+export { NotFound };
