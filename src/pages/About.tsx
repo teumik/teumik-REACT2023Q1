9405544120a6f05fa96df54ec507bb079c8bfff6
Artem Mokhov <teumik@gmail.com>
2023-03-17 20:43:20 +0400

    feat: implement About component

---
+++ src/pages/About.tsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <>
+        <h1>About Us</h1>
+        <p>Text</p>
+      </>
+    );
+  }
+}
+
+export { About };
