50a443d0a5470e65e0517e68fcd8eb74c39cca01
Artem Mokhov <teumik@gmail.com>
2023-03-18 16:52:07 +0400

    feat: add title in page header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,58 @@
 import { Component, PropsWithChildren } from 'react';
+import { Link } from 'react-router-dom';
 import style from './header.module.scss';
+import { ReactLogo } from '../ReactLogo';
+import { Navigation } from '../Navigation';
+import { paths } from '../../routers/paths';
 
-class Header extends Component<PropsWithChildren> {
+interface HeaderState {
+  title: string;
+}
+
+class Header extends Component<PropsWithChildren, HeaderState> {
+  constructor(props: PropsWithChildren) {
+    super(props);
+    this.state = {
+      title: '',
+    };
+  }
+
+  componentDidMount(): void {
+    this.titleHandler();
+    globalThis.onpopstate = this.titleHandler;
+  }
+
+  componentWillUnmount(): void {
+    globalThis.onpopstate = null;
+  }
+
+  getCurrentTitle = () => {
+    const { hash } = globalThis.location;
+    const path = hash.slice(1);
+    const title = paths.get(path);
+    return title || '';
+  };
+
+  titleHandler = () => {
+    const title = this.getCurrentTitle();
+    this.setTitle(title);
+  };
+
+  setTitle = (title: string) => {
+    this.setState((state) => ({ ...state, title }));
+  };
+
   render() {
-    const { children } = this.props;
-    return <header className={style.header}>{children}</header>;
+    const { title } = this.state;
+    return (
+      <header className={style.header}>
+        <Link to="https://react.dev/">
+          <ReactLogo />
+        </Link>
+        <h2>{title}</h2>
+        <Navigation setTitle={this.setTitle} />
+      </header>
+    );
   }
 }
 
