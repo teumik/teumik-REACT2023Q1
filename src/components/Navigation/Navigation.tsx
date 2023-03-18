4dad34b2b92e290b6e63b5a72507f165b1847cab
Artem Mokhov <teumik@gmail.com>
2023-03-18 16:52:58 +0400

    feat: implement setter for page title

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,17 +2,22 @@ import { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import style from './navigation.module.scss';
 
-class Navigation extends Component {
+interface NavigationProps {
+  setTitle: (title: string) => void;
+}
+
+class Navigation extends Component<NavigationProps> {
   setActiveStyle = ({ isActive }: { isActive: boolean }) =>
     isActive ? `${style.link} ${style.active}` : style.link;
 
   render() {
+    const { setTitle } = this.props;
     return (
       <nav className={style.nav}>
-        <NavLink to="/" className={this.setActiveStyle}>
+        <NavLink to="/" className={this.setActiveStyle} onClick={() => setTitle('Home')}>
           Home
         </NavLink>
-        <NavLink to="/about" className={this.setActiveStyle}>
+        <NavLink to="/about" className={this.setActiveStyle} onClick={() => setTitle('About Us')}>
           About Us
         </NavLink>
       </nav>
