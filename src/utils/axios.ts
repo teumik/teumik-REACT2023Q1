95544964cf3a80bcee7e72f24ef680abab18585c
Artem Mokhov <teumik@gmail.com>
2023-03-19 15:40:00 +0400

    fix: netify cors policy

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,13 @@
 type Axios = <T>(url: string) => Promise<T>;
 
 const axios: Axios = (url) =>
-  fetch(url)
+  fetch(url, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTION',
+      'Content-Type': 'application/json',
+    },
+  })
     .then((res) => res.json())
     .then((json) => json);
 
