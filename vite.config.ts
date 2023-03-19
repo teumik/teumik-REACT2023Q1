c092140fb725afba95fe2725c6a1d6a09aefbeb7
Artem Mokhov <teumik@gmail.com>
2023-03-19 13:28:20 +0400

    feat: setup testing enviroment and packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
@@ -10,4 +13,9 @@ export default defineConfig({
       typescript: true,
     }),
   ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+  },
 });
