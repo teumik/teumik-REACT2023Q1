c5a2c27fe180d737a757122e8c17748d4479bcf4
Artem Mokhov <teumik@gmail.com>
2023-03-19 13:27:15 +0400

    refactor; update lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -58,6 +58,7 @@ module.exports = {
       },
     ],
     'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
 
     indent: 'off',
     'no-shadow': 'off',
