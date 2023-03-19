0edfd57ee9449a7a7c79cb5e943f972c6aa5c51e
Artem Mokhov <teumik@gmail.com>
2023-03-19 15:01:44 +0400

    fix: add no-cors mode

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 type Axios = <T>(url: string) => Promise<T>;
 
 const axios: Axios = (url) =>
-  fetch(url)
+  fetch(url, { mode: 'no-cors' })
     .then((res) => res.json())
     .then((json) => json);
 
