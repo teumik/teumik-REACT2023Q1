1bdd31393f9b7acd9434f3ed7d47be678c8961d4
Artem Mokhov <teumik@gmail.com>
2023-03-19 13:29:50 +0400

    feat: change router type

--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -1,14 +1,14 @@
-import { Route, createHashRouter, createRoutesFromElements } from 'react-router-dom';
+import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import { Layout } from '../pages/Layout';
 import { NotFound } from '../pages/NotFound';
 import { Home } from '../pages/Home';
 import { About } from '../pages/About';
 
-const router = createHashRouter(
+const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
-      <Route path="/about" element={<About />} />
+      <Route path="about" element={<About />} />
       <Route path="*" element={<NotFound />} />
     </Route>
   )
