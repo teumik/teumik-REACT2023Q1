aad6460d50c254201b8d672600835aefa696d691
Artem Mokhov <teumik@gmail.com>
2023-03-22 21:42:26 +0400

    init: start second task

--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -3,12 +3,14 @@ import { Layout } from '../pages/Layout';
 import { NotFound } from '../pages/NotFound';
 import { Home } from '../pages/Home';
 import { About } from '../pages/About';
+import { Forms } from '../pages/Forms';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
+      <Route path="forms" element={<Forms />} />
       <Route path="*" element={<NotFound />} />
     </Route>
   )
