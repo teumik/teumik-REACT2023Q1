8dedf6aa58e475f0b160aa305513a1517fe18ff1
Artem Mokhov <teumik@gmail.com>
2023-03-27 03:00:04 +0400

    feat: implement Forms page tests

---
+++ src/pages/Forms.test.tsx
@@ -0,0 +1,14 @@
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { Forms } from './Forms';
+
+describe('Forms', () => {
+  it('Test render NotFound', () => {
+    render(<Forms />);
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Forms');
+  });
+});
