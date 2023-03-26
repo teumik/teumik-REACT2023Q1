1506d31008c7d5b040737d6fe425b3086bbe2738
Artem Mokhov <teumik@gmail.com>
2023-03-26 13:11:55 +0400

    feat: implement birth date component

---
+++ src/components/CustomForm/BirthDateInput/BirthDateInput.tsx
@@ -0,0 +1,31 @@
+import { Component, RefObject } from 'react';
+import { ErrorMessage } from '../ErrorMessage/ErrorMessage';
+
+interface BirthDateInputProps {
+  birthDateRef: RefObject<HTMLInputElement>;
+  error: [boolean, string];
+}
+
+class BirthDateInput extends Component<BirthDateInputProps> {
+  render() {
+    const {
+      birthDateRef,
+      error: [isError, errorMessage],
+    } = this.props;
+
+    return (
+      <>
+        <label htmlFor="date">Birth Date:</label>
+        <input type="date" name="date" id="date" ref={birthDateRef} />
+        {isError && (
+          <>
+            <span />
+            <ErrorMessage message={errorMessage} />
+          </>
+        )}
+      </>
+    );
+  }
+}
+
+export { BirthDateInput };
