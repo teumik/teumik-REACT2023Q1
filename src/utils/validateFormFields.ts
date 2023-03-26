48baafac18176485ea8e982df4fc6f26fee4c741
Artem Mokhov <teumik@gmail.com>
2023-03-27 03:00:59 +0400

    refactor: validation methods

--- src/utils/validateFormFields.ts
+++ src/utils/validateFormFields.ts
@@ -12,18 +12,15 @@ interface ReturnAction {
 class FormValidation {
   private validatePolicy = (field: RefObject<HTMLInputElement>) => {
     const node = field.current;
-    if (!node) return { ok: false, type: 'node' };
-    if (!node.checked) return { ok: false, type: 'Field must be checked' };
+    if (!node?.checked) return { ok: false, type: 'Field must be checked' };
     return { ok: true, type: '' };
   };
 
   private validateImage = (field: RefObject<HTMLInputElement>) => {
     const imageError = { ok: false, type: 'Upload image' };
     const node = field.current;
-    if (!node) return { ok: false, type: 'node' };
-    const { files } = node;
-    if (files?.length === 0) return imageError;
-    const image = files?.[0];
+    if (node?.files?.length === 0) return imageError;
+    const image = node?.files?.[0];
     const type = image?.type.split('/')?.[0];
     if (type !== 'image') return imageError;
     return { ok: true, type: '' };
@@ -31,19 +28,15 @@ class FormValidation {
 
   private validateCountry = (field: RefObject<HTMLSelectElement>) => {
     const node = field.current;
-    if (!node) return { ok: false, type: 'node' };
-    const { value } = node;
-    if (value === '') return { ok: false, type: 'Field cannot be empty' };
+    if (node?.value === '') return { ok: false, type: 'Field cannot be empty' };
     return { ok: true, type: '' };
   };
 
   private validateGenders = (field: RefObject<HTMLDivElement>) => {
     const node = field.current;
-    if (!node) return { ok: false, type: 'node' };
-    const isChecked = [...node.children].some((el) => {
+    const isChecked = Array.from((node as HTMLDivElement).children).some((el) => {
       const [option] = el.children;
-      if (!(option instanceof HTMLInputElement)) return false;
-      if (option.checked) return true;
+      if ((<HTMLInputElement>option)?.checked) return true;
       return false;
     });
     if (!isChecked) return { ok: false, type: 'Field cannot be empty' };
@@ -52,25 +45,28 @@ class FormValidation {
 
   private validateBirthDate = (field: RefObject<HTMLInputElement>) => {
     const node = field.current;
-    if (!node) return { ok: false, type: 'node' };
-    const { value } = node;
-    if (value === '') return { ok: false, type: 'Field cannot be empty' };
+    if (node?.value === '') return { ok: false, type: 'Field cannot be empty' };
     const currentDate = getCurrentDate();
-    const { year: currentyear } = getParseDate(currentDate);
-    const { year } = getParseDate(value);
-    if (new Date(currentDate) < new Date(value)) {
+    const { year: currentYear, month: currentMonth, day: currentDay } = getParseDate(currentDate);
+    const { year, month, day } = getParseDate(node?.value);
+    if (currentYear < year) {
       return { ok: false, type: 'You are not birth in future' };
     }
-    if (+year < +currentyear - 120) return { ok: false, type: 'You are not immortal' };
+    if (currentYear === year && currentMonth < month) {
+      return { ok: false, type: 'You are not birth in future' };
+    }
+    if (currentYear === year && currentMonth === month && currentDay < day) {
+      return { ok: false, type: 'You are not birth in future' };
+    }
+    if (Number(year) < Number(currentYear) - 120)
+      return { ok: false, type: 'You are not immortal' };
     return { ok: true, type: '' };
   };
 
   private validateFullName = (field: RefObject<HTMLInputElement>) => {
     const node = field.current;
-    if (!node) return { ok: false, type: 'node' };
-    const { value } = node;
-    if (value === '') return { ok: false, type: 'Field cannot be empty' };
-    const isCapitalized = value === capitalize(value);
+    if (node?.value === '') return { ok: false, type: 'Field cannot be empty' };
+    const isCapitalized = node?.value === capitalize(node?.value);
     if (!isCapitalized) return { ok: false, type: 'First letter must be capital' };
     return { ok: true, type: '' };
   };
