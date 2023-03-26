be6a5a8a199d8cf01bcfba3ff9e35cac47487912
Artem Mokhov <teumik@gmail.com>
2023-03-26 13:14:13 +0400

    feat: implement container for form and cards

---
+++ src/components/FormCards/FormCards.tsx
@@ -0,0 +1,40 @@
+import { Component, ReactPropTypes } from 'react';
+import style from './formCards.module.scss';
+import { CardItem, FormCardItem } from '../FormCardItem/FormCardItem';
+import { CustomForm } from '../CustomForm/CustomForm';
+
+interface FormCardsState {
+  cards: CardItem[];
+}
+
+class FormCards extends Component<Partial<ReactPropTypes>, FormCardsState> {
+  constructor(props: ReactPropTypes) {
+    super(props);
+    this.state = {
+      cards: [],
+    };
+  }
+
+  addCard = (card: CardItem) => {
+    this.setState((state) => ({
+      ...state,
+      cards: [...state.cards, { ...card }],
+    }));
+  };
+
+  render() {
+    const { cards } = this.state;
+    return (
+      <>
+        <CustomForm addCard={this.addCard} />
+        <section className={style['cards-container']}>
+          {cards.map((card) => (
+            <FormCardItem key={Math.random()} cardData={card} />
+          ))}
+        </section>
+      </>
+    );
+  }
+}
+
+export { FormCards };
