c3198dc8b04f46d347b9a9ddda40a15ee67d7bca
Artem Mokhov <teumik@gmail.com>
2023-03-30 01:49:48 +0400

    feat: implement users hooks

---
+++ src/hooks/useFetchProducts.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+import { Product } from '../components/ProductItem/ProductItem';
+import { getProductFromFile } from '../utils/getProductFromFile';
+import { paths } from '../routers/Paths';
+
+interface ProductsResponse {
+  products: Product[];
+}
+
+const useFetchProducts = () => {
+  const [products, setProducts] = useState([] as Product[]);
+
+  useEffect(() => {
+    getProductFromFile<ProductsResponse>(paths.serverUrl).then(({ products: productsList }) => {
+      setProducts(productsList);
+    });
+  }, []);
+
+  return { products };
+};
+
+export { useFetchProducts };
