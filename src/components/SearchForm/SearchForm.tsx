52e9b2a168e9cd32c73db2d4fce36e0b3774fc8d
Artem Mokhov <teumik@gmail.com>
2023-03-31 11:17:36 +0400

    feat: replace class on function component

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,84 +1,56 @@
-import { ChangeEvent, Component, FormEvent } from 'react';
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import { SearchLogo } from '../SearchLogo/SearchLogo';
 import style from './searchForm.module.scss';
-
-interface SearchState {
-  search: string;
-}
+import { useLocalStorage } from '../../hooks/useLocalStorage';
 
 interface SearchProps {
   setQuery: (query: string) => void;
 }
 
-class SearchForm extends Component<SearchProps, SearchState> {
-  constructor(props: SearchProps) {
-    super(props);
-    this.state = {
-      search: this.localStorageValue(),
-    };
-  }
+function SearchForm({ setQuery }: SearchProps) {
+  const { getStorageValue, storageEffect } = useLocalStorage('search');
+  const [search, setSearch] = useState(getStorageValue());
 
-  componentDidMount(): void {
-    const search = this.localStorageValue();
-    this.setSearch(search);
-    const { setQuery } = this.props;
+  useEffect(() => {
     setQuery(search);
-    globalThis.addEventListener('beforeunload', this.saveStateToLocalStorage);
-  }
+  });
 
-  componentWillUnmount(): void {
-    this.saveStateToLocalStorage();
-    globalThis.removeEventListener('beforeunload', this.saveStateToLocalStorage);
-  }
+  useEffect(() => storageEffect(search));
 
-  saveStateToLocalStorage = () => {
-    const { search } = this.state;
-    globalThis.localStorage.setItem('search', search);
-  };
-
-  localStorageValue = () => globalThis.localStorage.getItem('search') || '';
-
-  onSubmit = (event: FormEvent<HTMLFormElement> & { target: HTMLFormElement }) => {
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const { search } = this.state;
-    const { setQuery } = this.props;
     setQuery(search);
   };
 
-  onChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
-    this.setSearch(value);
+    setSearch(value);
   };
 
-  setSearch = (search: string) => this.setState((state) => ({ ...state, search }));
-
-  render() {
-    const { search } = this.state;
-    return (
-      <div className={style.container}>
-        <form
-          name="search-form"
-          onSubmit={this.onSubmit}
-          className={style.form}
+  return (
+    <div className={style.container}>
+      <form
+        name="search-form"
+        onSubmit={onSubmit}
+        className={style.form}
+      >
+        <input
+          type="text"
+          name="search"
+          placeholder="Search"
+          spellCheck="false"
+          value={search}
+          onChange={onChange}
+        />
+        <button
+          type="submit"
+          className={style.button}
         >
-          <input
-            type="text"
-            name="search"
-            placeholder="Search"
-            spellCheck="false"
-            value={search}
-            onChange={this.onChange}
-          />
-          <button
-            type="submit"
-            className={style.button}
-          >
-            <SearchLogo />
-          </button>
-        </form>
-      </div>
-    );
-  }
+          <SearchLogo />
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export { SearchForm };
