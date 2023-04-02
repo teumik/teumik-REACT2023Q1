896094c424ba046e803f10488fa48221723c17a1
Artem Mokhov <teumik@gmail.com>
2023-04-02 23:08:15 +0400

    fix: custom form test

--- src/components/CustomForm/CustomForm.test.tsx
+++ src/components/CustomForm/CustomForm.test.tsx
@@ -1,64 +1,104 @@
 import { beforeEach, describe, it, vi } from 'vitest';
-import { act, fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import { CustomForm } from './CustomForm';
+
+const getFakeDate = (action: string) => {
+  const getByValue = (ms: number) => new Date(Date.now() + ms).toISOString().split('T');
+  switch (action) {
+    case 'plus year': {
+      const value = 1000 * 60 * 60 * 24 * 365;
+      const [date] = getByValue(value);
+      return date;
+    }
+    case 'plus month': {
+      const value = 1000 * 60 * 60 * 24 * 30;
+      const [date] = getByValue(value);
+      return date;
+    }
+    case 'plus day': {
+      const value = 1000 * 60 * 60 * 24;
+      const [date] = getByValue(value);
+      return date;
+    }
+    default:
+      return '0';
+  }
+};
 
 beforeEach(async () => {
   await act(async () => {
     const handler = vi.fn();
+    globalThis.URL.createObjectURL = vi.fn();
     render(<CustomForm addCard={handler} />);
   });
 });
 
 describe('CustomForm', () => {
-  it('Test incorrect first name (empty)', () => {
-    fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: '' } });
-    fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+  it('Test incorrect first name (empty)', async () => {
+    await act(async () => {
+      fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: '' } });
+      fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+    });
   });
-  it('Test incorrect first name (lower case)', () => {
-    fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'ame' } });
-    fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+  it('Test incorrect first name (lower case)', async () => {
+    await act(async () => {
+      fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'ame' } });
+      fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+    });
   });
-  it('Test correct first name', () => {
-    fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
-    fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+  it('Test correct first name', async () => {
+    await act(async () => {
+      fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
+      fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+    });
   });
-  it('Test incorrect surname (empty)', () => {
-    fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
-    fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: '' } });
-    fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+  it('Test incorrect surname (empty)', async () => {
+    await act(async () => {
+      fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
+      fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: '' } });
+      fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+    });
   });
-  it('Test incorrect surname (lower case)', () => {
-    fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
-    fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: 'urname' } });
-    fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+  it('Test incorrect surname (lower case)', async () => {
+    await act(async () => {
+      fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
+      fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: 'urname' } });
+      fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+    });
   });
-  it('Test correct surname', () => {
-    fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
-    fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: 'Surname' } });
-    fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+  it('Test correct surname', async () => {
+    await act(async () => {
+      fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
+      fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: 'Surname' } });
+      fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+    });
   });
 
   it('Test incorrect date', async () => {
+    const fakeDate = getFakeDate('plus year');
     await act(async () => {
       fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
       fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: 'Surname' } });
-      fireEvent.change(screen.getByTestId('date'), { target: { value: '9999-01-01' } });
+      fireEvent.change(screen.getByTestId('date'), { target: { value: fakeDate } });
       fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
     });
   });
   it('Test incorrect date', async () => {
+    const fakeDate = getFakeDate('plus month');
     await act(async () => {
       fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
       fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: 'Surname' } });
-      fireEvent.change(screen.getByTestId('date'), { target: { value: '2023-12-31' } });
+      fireEvent.change(screen.getByTestId('date'), { target: { value: fakeDate } });
       fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
     });
   });
   it('Test incorrect date', async () => {
+    const fakeDate = getFakeDate('plus day');
     await act(async () => {
       fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
       fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: 'Surname' } });
-      fireEvent.change(screen.getByTestId('date'), { target: { value: '2023-03-31' } });
+      fireEvent.change(screen.getByTestId('date'), { target: { value: fakeDate } });
       fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
     });
   });
@@ -79,89 +119,80 @@ describe('CustomForm', () => {
     });
   });
 
-  it('Test country select (empty)', () => {
-    fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
-    fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: 'Surname' } });
-    fireEvent.change(screen.getByTestId('date'), { target: { value: '2022-01-01' } });
-    fireEvent.change(screen.getByRole('combobox'), { target: { value: '' } });
-    fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+  it('Test country select (empty)', async () => {
+    await act(async () => {
+      fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
+      fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: 'Surname' } });
+      fireEvent.change(screen.getByTestId('date'), { target: { value: '2022-01-01' } });
+      fireEvent.change(screen.getByRole('combobox'), { target: { value: '' } });
+      fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+    });
   });
-  it('Test country select (empty)', () => {
-    fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
-    fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: 'Surname' } });
-    fireEvent.change(screen.getByTestId('date'), { target: { value: '2022-01-01' } });
-    fireEvent.change(screen.getByRole('combobox'), { target: { value: 'russia' } });
-    fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+  it('Test country select (empty)', async () => {
+    await act(async () => {
+      fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
+      fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: 'Surname' } });
+      fireEvent.change(screen.getByTestId('date'), { target: { value: '2022-01-01' } });
+      fireEvent.change(screen.getByRole('combobox'), { target: { value: 'russia' } });
+      fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+    });
   });
-  it('Test correct radio', () => {
-    fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
-    fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: 'Surname' } });
-    fireEvent.change(screen.getByTestId('date'), { target: { value: '2022-01-01' } });
-    fireEvent.change(screen.getByRole('combobox'), { target: { value: 'russia' } });
-    fireEvent.click(screen.getAllByRole('radio')[0]);
-    fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+  it('Test correct radio', async () => {
+    await act(async () => {
+      fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
+      fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: 'Surname' } });
+      fireEvent.change(screen.getByTestId('date'), { target: { value: '2022-01-01' } });
+      fireEvent.change(screen.getByRole('combobox'), { target: { value: 'russia' } });
+      fireEvent.click(screen.getAllByRole('radio')[0]);
+      fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+    });
   });
-  it('Test correct file type', () => {
+  it('Test correct file type', async () => {
     const file = new File(['image'], 'image.jpeg', { type: 'image/jpeg' });
-    fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
-    fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: 'Surname' } });
-    fireEvent.change(screen.getByTestId('date'), { target: { value: '2022-01-01' } });
-    fireEvent.click(screen.getAllByRole('radio')[0]);
-    fireEvent.change(screen.getByRole('combobox'), { target: { value: 'russia' } });
-    fireEvent.change(screen.getByTestId('file'), { target: { files: [file] } });
-    fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+    await act(async () => {
+      fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
+      fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: 'Surname' } });
+      fireEvent.change(screen.getByTestId('date'), { target: { value: '2022-01-01' } });
+      fireEvent.click(screen.getAllByRole('radio')[0]);
+      fireEvent.change(screen.getByRole('combobox'), { target: { value: 'russia' } });
+      fireEvent.change(screen.getByTestId('file'), { target: { files: [file] } });
+      fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+    });
   });
-  it('Test incorrect file type', () => {
+  it('Test incorrect file type', async () => {
     const file = new File(['text'], 'text.txt', { type: 'text/plain' });
-    fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
-    fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: 'Surname' } });
-    fireEvent.change(screen.getByTestId('date'), { target: { value: '2022-01-01' } });
-    fireEvent.click(screen.getAllByRole('radio')[0]);
-    fireEvent.change(screen.getByRole('combobox'), { target: { value: 'russia' } });
-    fireEvent.change(screen.getByTestId('file'), { target: { files: [file] } });
-    fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+    await act(async () => {
+      fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
+      fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: 'Surname' } });
+      fireEvent.change(screen.getByTestId('date'), { target: { value: '2022-01-01' } });
+      fireEvent.click(screen.getAllByRole('radio')[0]);
+      fireEvent.change(screen.getByRole('combobox'), { target: { value: 'russia' } });
+      fireEvent.change(screen.getByTestId('file'), { target: { files: [file] } });
+      fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+    });
   });
-  it('Test without file', () => {
-    fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
-    fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: 'Surname' } });
-    fireEvent.change(screen.getByTestId('date'), { target: { value: '2022-01-01' } });
-    fireEvent.click(screen.getAllByRole('radio')[0]);
-    fireEvent.change(screen.getByRole('combobox'), { target: { value: 'russia' } });
-    fireEvent.change(screen.getByTestId('file'), { target: { files: [undefined] } });
-    fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+  it('Test without file', async () => {
+    await act(async () => {
+      fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
+      fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: 'Surname' } });
+      fireEvent.change(screen.getByTestId('date'), { target: { value: '2022-01-01' } });
+      fireEvent.click(screen.getAllByRole('radio')[0]);
+      fireEvent.change(screen.getByRole('combobox'), { target: { value: 'russia' } });
+      fireEvent.change(screen.getByTestId('file'), { target: { files: [undefined] } });
+      fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+    });
   });
-  it('Test policy checkbox', () => {
+  it('Test policy checkbox', async () => {
     const file = new File(['image'], 'image.jpeg', { type: 'image/jpeg' });
-    fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
-    fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: 'Surname' } });
-    fireEvent.change(screen.getByTestId('date'), { target: { value: '2022-01-01' } });
-    fireEvent.click(screen.getAllByRole('radio')[0]);
-    fireEvent.change(screen.getByRole('combobox'), { target: { value: 'russia' } });
-    fireEvent.change(screen.getByTestId('file'), { target: { files: [file] } });
-    fireEvent.click(screen.getByRole('checkbox'));
-    fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
-  });
-  describe('CustomForm', () => {
-    it('Test sendFormData', async () => {
-      const handler = vi.fn();
-      const instance = new CustomForm({ addCard: handler });
-      instance.sendFormData();
-      expect(handler).toBeCalledTimes(1);
-    });
-    it('Test resetForm', async () => {
-      const handler = vi.fn();
-      const instance = new CustomForm({ addCard: handler });
-      instance.resetForm();
-    });
-    it('Test setImageFile', async () => {
-      const handler = vi.fn();
-      const instance = new CustomForm({ addCard: handler });
-      Object.assign(instance.refsProps.image, undefined);
-      await waitFor(async () => {
-        await instance.setImageFile().then((result) => {
-          expect(result).toEqual(true);
-        });
-      });
+    await act(async () => {
+      fireEvent.change(screen.getAllByRole('textbox')[0], { target: { value: 'Name' } });
+      fireEvent.change(screen.getAllByRole('textbox')[1], { target: { value: 'Surname' } });
+      fireEvent.change(screen.getByTestId('date'), { target: { value: '2022-01-01' } });
+      fireEvent.click(screen.getAllByRole('radio')[0]);
+      fireEvent.change(screen.getByRole('combobox'), { target: { value: 'russia' } });
+      fireEvent.change(screen.getByTestId('file'), { target: { files: [file] } });
+      fireEvent.click(screen.getByRole('checkbox'));
+      fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
     });
   });
 });
