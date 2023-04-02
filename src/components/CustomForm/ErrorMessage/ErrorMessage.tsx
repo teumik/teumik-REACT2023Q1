ade5e53a31d5bc2de5001addfc8848dcf11f4220
Artem Mokhov <teumik@gmail.com>
2023-04-02 12:40:59 +0400

    feat: replace class on function component

--- src/components/CustomForm/ErrorMessage/ErrorMessage.tsx
+++ src/components/CustomForm/ErrorMessage/ErrorMessage.tsx
@@ -1,22 +1,18 @@
-import { Component } from 'react';
 import style from './errorMessage.module.scss';
 
 interface ErrorMessageProps {
   message: string | undefined;
 }
 
-class ErrorMessage extends Component<ErrorMessageProps> {
-  render() {
-    const { message } = this.props;
-    return (
-      <div
-        className={style.error}
-        data-testid="message"
-      >
-        {message || ''}
-      </div>
-    );
-  }
+function ErrorMessage({ message }: ErrorMessageProps) {
+  return (
+    <div
+      className={style.error}
+      data-testid="message"
+    >
+      {message || ''}
+    </div>
+  );
 }
 
 export { ErrorMessage };
