a8aa1c0ba1d976a8cdbe068c6ae0fce7edcf6052
Artem Mokhov <teumik@gmail.com>
2023-04-07 22:22:17 +0400

    feat: replace class component to function

--- src/components/BackButton/BackButton.tsx
+++ src/components/BackButton/BackButton.tsx
@@ -1,22 +1,15 @@
-import { Component } from 'react';
 import style from './backButton.module.scss';
 
-class BackButton extends Component {
-  private back = () => {
-    globalThis.history.back();
-  };
-
-  render() {
-    return (
-      <button
-        type="button"
-        className={style.button}
-        onClick={this.back}
-      >
-        Back
-      </button>
-    );
-  }
+function BackButton() {
+  return (
+    <button
+      type="button"
+      className={style.button}
+      onClick={() => globalThis.history.back()}
+    >
+      Back
+    </button>
+  );
 }
 
 export { BackButton };
