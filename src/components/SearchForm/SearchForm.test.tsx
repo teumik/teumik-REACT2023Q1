1ba98ede9dd507621ce0d674639355c524e0fe43
Artem Mokhov <teumik@gmail.com>
2023-04-07 21:54:05 +0400

    refactor: replace by screen

--- src/components/SearchForm/SearchForm.test.tsx
+++ src/components/SearchForm/SearchForm.test.tsx
@@ -1,29 +1,30 @@
 import { describe, it, vi } from 'vitest';
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { SearchForm } from './SearchForm';
 
 describe('SearchForm', () => {
   it('Test search input', () => {
     const handler = vi.fn();
-    const input = render(
+    render(
       <BrowserRouter>
         <SearchForm setQuery={handler} />
       </BrowserRouter>
-    ).getByRole('textbox') as HTMLInputElement;
-    expect(input).toBeInTheDocument();
+    );
+    const input = screen.getByRole<HTMLInputElement>('textbox');
+    expect(input.value).toBe('');
     fireEvent.change(input, { target: { value: 'mens' } });
     expect(input.value).toBe('mens');
   });
   it('Test search submit', () => {
     const handler = vi.fn();
-    const result = render(
+    render(
       <BrowserRouter>
         <SearchForm setQuery={handler} />
       </BrowserRouter>
     );
-    const form = result.getByRole('form');
-    const button = result.getByRole('button');
+    const form = screen.getByRole('form');
+    const button = screen.getByRole('button');
     expect(form).toBeInTheDocument();
     expect(button).toBeInTheDocument();
     fireEvent.click(button);
