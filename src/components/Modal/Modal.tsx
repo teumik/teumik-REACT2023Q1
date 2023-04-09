919818ded7384891c4f2b53a3bfd5a8b7c5b4189
Artem Mokhov <teumik@gmail.com>
2023-04-09 23:36:47 +0400

    feat: implement modal wrapper component

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,34 @@
+import { createPortal } from 'react-dom';
+import { PropsWithChildren } from 'react';
+import style from './Modal.module.scss';
+import { Preloader } from '../Preloader/Preloader';
+
+interface Props extends PropsWithChildren {
+  isPending: boolean;
+  showModal: boolean;
+  onClose: () => void;
+}
+
+function Modal({ isPending, showModal, onClose, children }: Props) {
+  if (!showModal) {
+    return null;
+  }
+
+  return createPortal(
+    <>
+      <button
+        type="button"
+        className={style.overlay}
+        onClick={onClose}
+        data-testid="overlay"
+      />
+      <div className={style.modal}>
+        {isPending && <Preloader />}
+        {children}
+      </div>
+    </>,
+    document.body
+  );
+}
+
+export { Modal };
