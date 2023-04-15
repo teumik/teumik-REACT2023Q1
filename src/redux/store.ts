43b0a919cd9679c2b66ecc4c0a889038c36cfb6e
Artem Mokhov <teumik@gmail.com>
2023-04-15 13:16:01 +0400

    feat: add separated combine reducers

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { formReducer } from './slices/formSlice';
 import { formCardsReducer } from './slices/formCardsSlice';
 
+const reducer = combineReducers({
+  form: formReducer,
+  formCards: formCardsReducer,
+});
+
 const store = configureStore({
-  reducer: {
-    form: formReducer,
-    formCards: formCardsReducer,
-  },
+  reducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
