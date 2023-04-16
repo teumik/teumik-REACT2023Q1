044b958cd0d8f7ed7aa74f92d06f064a99dd054b
Artem Mokhov <teumik@gmail.com>
2023-04-16 15:23:56 +0400

    refactor: wrap with provider and remove unused code

--- src/components/Products/__tests__/Products.test.tsx
+++ src/components/Products/__tests__/Products.test.tsx
@@ -1,50 +1,27 @@
-import { Mock, describe, it, vi } from 'vitest';
+import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { act } from 'react-dom/test-utils';
 import { Products } from '../Products';
-import { customFetch } from '../../../utils/customFetch';
-import { mockResponse } from '../../../__mocks__/response.mock';
-
-beforeAll(() => {
-  vi.mock('../../../utils/customFetch');
-  (customFetch as Mock).mockReturnValue(Promise.resolve(mockResponse));
-});
-
-afterAll(() => {
-  vi.restoreAllMocks();
-});
+import { store } from '../../../redux/store';
+import { fetchItems } from '../../../redux/slices/apiSlice';
+import { paths } from '../../../routers/Paths';
 
 describe('Products', () => {
-  it('Test render products', async () => {
-    const handler = vi.fn();
-    const { results } = mockResponse;
+  it('Test render products with error', async () => {
     render(
-      <Products
-        isPending={false}
-        notFoundMessage=""
-        searchItems={handler}
-        products={results}
-      />
+      <Provider store={store}>
+        <Products />
+      </Provider>
     );
     const titles = await screen.findAllByRole('heading', {
       level: 3,
     });
     expect(titles.length).toEqual(2);
-  });
-  it('Test render notFoundMessage', async () => {
-    const handler = vi.fn();
-    const { results } = mockResponse;
-    render(
-      <Products
-        isPending={false}
-        notFoundMessage="quaja"
-        searchItems={handler}
-        products={results}
-      />
-    );
-    const titles = await screen.findAllByRole('heading', {
-      level: 3,
+
+    await act(async () => {
+      await store.dispatch(fetchItems({ path: `${paths.serverUrl}aaa` }));
     });
-    expect(titles.length).toEqual(2);
-    expect(screen.getByText(/quaja/i)).toBeInTheDocument();
+    expect(screen.getByText(/NOT FOUND/i)).toBeInTheDocument();
   });
 });
