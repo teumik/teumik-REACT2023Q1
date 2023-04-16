601d7fc346c81ae0aa7a199f87ea1a0eeadcc3b2
Artem Mokhov <teumik@gmail.com>
2023-04-16 11:38:42 +0400

    refactor: rename reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { formReducer } from './slices/formSlice';
 import { formCardsReducer } from './slices/formCardsSlice';
+import { apiReducer } from './slices/apiSlice';
 
 const reducer = combineReducers({
   form: formReducer,
   formCards: formCardsReducer,
+  api: apiReducer,
 });
 
 const store = configureStore({
