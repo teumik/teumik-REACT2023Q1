d66dfbacbdfa035c73fbd284b85392679ba308b0
Artem Mokhov <teumik@gmail.com>
2023-04-16 15:22:35 +0400

    refactor: rename title

--- src/components/SearchLogo/__test__/SearchLogo.test.tsx
+++ src/components/SearchLogo/__test__/SearchLogo.test.tsx
@@ -2,8 +2,8 @@ import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { SearchLogo } from '../SearchLogo';
 
-describe('RatingLogo', () => {
-  it('Test render rating logo', () => {
+describe('SearchLogo', () => {
+  it('Test render search logo', () => {
     render(<SearchLogo />);
     expect(screen.getByRole('img')).toBeInTheDocument();
   });
