73cd4190961f0f3ce8198d895c3b83d65e2debcc
Artem Mokhov <teumik@gmail.com>
2023-04-30 21:19:49 +0400

    refactor: change folder name and change store structure

--- src/store/slices/__tests__/apiSlice.test.ts
+++ src/store/slices/__tests__/apiSlice.test.ts
@@ -1,9 +1,11 @@
 import { describe, it } from 'vitest';
 import { act } from 'react-dom/test-utils';
-import { store } from '../../store';
+import { createStore } from '../../store';
 import { fetchItemById } from '../apiSlice';
 
 describe('apiSlice', () => {
+  const store = createStore();
+
   it('Test implementation api slice', async () => {
     await act(async () => {
       await store.dispatch(fetchItemById({ id: 3 }));
--- src/store/slices/__tests__/formSlice.test.ts
+++ src/store/slices/__tests__/formSlice.test.ts
@@ -1,9 +1,11 @@
 import { describe, it } from 'vitest';
 import { act } from 'react-dom/test-utils';
-import { store } from '../../store';
+import { createStore } from '../../store';
 import { formAction } from '../formSlice';
 
 describe('formSlice', () => {
+  const store = createStore();
+
   it('Test implementation form slice', async () => {
     act(() => {
       store.dispatch(formAction.setValue({ fields: { firstName: 'Name' } }));
