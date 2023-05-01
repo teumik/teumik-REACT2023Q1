233a90fa113442ef3b1f979538f9584572133d44
Artem Mokhov <teumik@gmail.com>
2023-05-01 06:29:05 +0400

    feat: implement link to home page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,14 +1,15 @@
+import { Link } from 'react-router-dom';
 import { BackButton } from '../components/BackButton/BackButton';
 
 function NotFound() {
   return (
     <>
-      <div>
+      <Link to="/">
         <BackButton />
-      </div>
+      </Link>
       <div>
-        <h1>404</h1>
-        <h2>Page Not Found</h2>
+        <h1 data-cy="header">404</h1>
+        <h2 data-cy="description">Page Not Found</h2>
       </div>
     </>
   );
--- src/pages/__tests__/NotFound.test.tsx
+++ src/pages/__tests__/NotFound.test.tsx
@@ -1,10 +1,15 @@
 import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import { NotFound } from '../NotFound';
 
 describe('NotFound', () => {
   it('Test render NotFound', () => {
-    render(<NotFound />);
+    render(
+      <BrowserRouter>
+        <NotFound />
+      </BrowserRouter>
+    );
     expect(
       screen.getByRole('heading', {
         level: 1,
